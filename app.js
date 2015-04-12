27ac223cf26ea8471cd765693ac59e70c7276524
Chuck Langford <field.email@gmail.com>
2015-04-12 12:56:11 -0400

    Spelling correction

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31536000000})); //Note: expressuses milliseconds to set maxAge, browsers display maxAge in seconds
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31536000000})); //Note: express uses milliseconds to set maxAge, browsers display maxAge in seconds
 
 app.use('/', routes);
 app.use('/users', users);
@@ -55,6 +55,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
