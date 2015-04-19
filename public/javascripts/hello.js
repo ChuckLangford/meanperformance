c0c0277a6588eaeaed87baa8413d4afc3534796f
Chuck Langford <field.email@gmail.com>
2015-04-19 09:13:07 -0400

    Added variables

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,4 +1,4 @@
-var hello = function() {
+var hello = function(variableName1, variableName2) {
   var name = document.getElementById('name').value;
   document.getElementById('message').innerHTML = name ?
     'Hello ' + name + '.' :
